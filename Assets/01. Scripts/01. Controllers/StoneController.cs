8ea4d67645ea6f7b4331b4d44f9154271b621c21
hyeona <90515062+BaeHyeonA@users.noreply.github.com>
2024-04-17 20:44:11 +0900

    :hammer: feat: add interaction zone

--- Assets/01. Scripts/01. Controllers/StoneController.cs
+++ Assets/01. Scripts/01. Controllers/StoneController.cs
@@ -1,54 +1,103 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class StoneController : MonoBehaviour
 {
-    private float dragStartTime;
-    private bool isDragging;
-    public float maxDragTime = 3.0f; //마우스 드래그 시간(최대)
-    private Rigidbody rb;
+    public bool isInZone;
+    private bool isDrop;
+    public bool speedDrop;
+    private Vector3 originPosition;
+    private Vector3 lastPosition;
+    private Rigidbody2D rb;
 
     void Start()
     {
-        GameManager.Input.keyAction -= Draged;
-        GameManager.Input.keyAction += Draged;
-        rb = GetComponent<Rigidbody>();
-        rb.useGravity = false;
+        originPosition = transform.position;
+        lastPosition = transform.position;
+        rb = GetComponent<Rigidbody2D>();
+        //GameManager.Input.keyAction -= Draged;
+        //GameManager.Input.keyAction += Draged;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (isDragging)
-        {
-            float duration = Time.time - dragStartTime;
-            if (duration > maxDragTime)
-                ReleaseObject();
-        }
     }
 
-    void Draged()
-    {
-        if (!isDragging)
-        {
-            isDragging = true;
-            dragStartTime = Time.time;
-            rb.useGravity = true;
-        }
+    // void Draged()
+    // {
+    //     RaycastHit hit;
+    //     Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    //     if (Physics.Raycast(ray, out hit))
+    //     {
+    //         // 오브젝트가 선택되었는지 확인
+    //         if (hit.transform.gameObject == transform.gameObject)
+    //         {
+    //             Debug.Log("In");
+    //             float distance = GameManager.Input.mousePosUnity.z;
+    //             Vector3 mousePosition = new Vector3(GameManager.Input.mousePosDevice.x, GameManager.Input.mousePosDevice.y, distance);
+    //             Vector3 objPosition = GameManager.Input.mousePosUnity;
+    //             transform.position = objPosition;
+    //         }
+    //     }
+    // }
 
-        if (isDragging)
+    void OnMouseDown()
+    {
+        lastPosition = transform.position;
+        isDrop = false;
+    }
+
+    void OnMouseDrag()
+    {
+        if(!speedDrop)
         {
             float distance = GameManager.Input.mousePosUnity.z;
             Vector3 mousePosition = new Vector3(GameManager.Input.mousePosDevice.x, GameManager.Input.mousePosDevice.y, distance);
             Vector3 objPosition = GameManager.Input.mousePosUnity;
-            transform.position = objPosition;
+
+            //float speed = (transform.position - lastPosition).magnitude / Time.deltaTime * 10000;
+
+            if (rb.velocity.magnitude < 0.7f){
+                transform.position = objPosition;
+                lastPosition = transform.position;
+            }
+            else
+            {
+                lastPosition = transform.position;
+                speedDrop = true;
+                StartCoroutine(Delay());
+            }
+
+            lastPosition = objPosition;
+            Debug.Log(rb.velocity.magnitude);
         }
     }
 
-    void ReleaseObject()
+    void OnMouseUp()
     {
-        isDragging = false;
-        rb.useGravity = false;
+        isDrop = true;
+    }
+
+    private void OnTriggerStay2D(Collider2D other) 
+    {
+        if(other.CompareTag("InteractionZone"))
+        {
+            if(isInZone && isDrop)
+            {
+                other.GetComponent<InteractionZone>().OpenMap();
+                isInZone = false;
+                isDrop = false;
+                speedDrop = false;
+                transform.position = originPosition;
+            }
+        }
+    }
+
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(2f);
+        speedDrop = false;
     }
 }
