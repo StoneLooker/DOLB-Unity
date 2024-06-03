9cebb0cfabeb4f3512c871046c4810331c753153
tinon1004 <tinon1004@ajou.ac.kr>
2024-06-03 11:11:49 +0900

    Feat: script for searching User

---
+++ Assets/01. Scripts/07. CollectingBook/SearchingUserManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Text;
+using TMPro;
+
+public class SearchingUserManager : MonoBehaviour
+{
+    private string searchUserUrl = "http://localhost:8080/search";
+
+    public TMP_InputField searchUserInputField;
+
+    public void SearchUser()
+    {
+        StartCoroutine(SearchUserRequest());
+    }
+
+    string getMemberNickNameFromField()
+    {
+        return searchUserInputField.text;
+    }
+
+    IEnumerator SearchUserRequest()
+    {
+        string memberNickName = getMemberNickNameFromField();
+        string url = $"{searchUserUrl}?memberNickName={memberNickName}";
+        UnityWebRequest www = UnityWebRequest.Get(url);
+        yield return www.SendWebRequest();
+
+        switch (www.result)
+        {
+            case UnityWebRequest.Result.ConnectionError:
+            case UnityWebRequest.Result.DataProcessingError:
+                Debug.Log("Error: " + www.error);
+                break;
+            case UnityWebRequest.Result.ProtocolError:
+                Debug.Log("HTTP Error: " + www.error + " Response: " + www.downloadHandler.text);
+                break;
+            case UnityWebRequest.Result.Success:
+                Debug.Log("Success: " + www.downloadHandler.text);
+                break;
+        }
+    }
+
+}
