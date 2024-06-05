00c4e88b1c43589236194ca2534b43373aa9b3f8
anbak98 <anbak98@gmail.com>
2024-06-06 01:06:58 +0900

    Add Method: CollectiongBookManager SpawnStone, JsonToString

--- Assets/01. Scripts/07. CollectingBook/CollectingBookManager.cs
+++ Assets/01. Scripts/07. CollectingBook/CollectingBookManager.cs
@@ -5,12 +5,16 @@ using UnityEngine.UI;
 using UnityEngine.Networking;
 using System.Text;
 using TMPro;
+using UnityEditor;
 
 public class CollectingBookManager : MonoBehaviour
 {
     private string addStoneUrl = "http://43.203.76.106:8080/collectingbook/add";
     private string getStoneUrl = "http://43.203.76.106:8080/collectingbook";
-   
+
+    [SerializeField] GameObject stoneFrame;
+
+    public List<CollectingBook> books { get; private set; }
 
     public TMP_InputField stoneNameInputField;
     public TMP_InputField stoneNumberInputField;
@@ -31,7 +35,40 @@ public class CollectingBookManager : MonoBehaviour
         StartCoroutine(GetStoneRequest());
     }
 
-  
+    public void SpawnStones()
+    {
+        GetStone();
+        Debug.Log("1");
+        foreach (CollectingBook element in books)
+        {
+            Debug.Log("2");
+            if (element.stoneName == "LimeStone")
+            {
+                Debug.Log(GameManager.Stone.collectingBook);
+                GameObject st = Instantiate(stoneFrame, new Vector3(0, 10, 0), new Quaternion(0, 0, 0, 0));
+                st.AddComponent<LimeStoneController>();
+
+            }
+        }
+    }
+
+    public List<CollectingBook> JsonToList(string jsonString)
+    {
+        jsonString = "{\"stones\":" + jsonString + "}";
+
+        CollectingBookList book = JsonUtility.FromJson<CollectingBookList>(jsonString);
+
+        // Convert the array to a list if needed
+        List<CollectingBook> stones = new List<CollectingBook>(book.stones);
+
+        // Now you can use the 'stones' list
+        foreach (var stone in stones)
+        {
+            Debug.Log("Name: " + stone.stoneName + ", Number: " + stone.stoneNumber);
+        }
+
+        return stones;
+    }
 
     IEnumerator AddStoneRequest()
     {
@@ -91,6 +128,7 @@ public class CollectingBookManager : MonoBehaviour
                 break;
             case UnityWebRequest.Result.Success:
                 Debug.Log("Success: " + www.downloadHandler.text);
+                books = JsonToList(www.downloadHandler.text);
                 break;
         }
     }
